0c92adb097b1149f879bcf6b83c11ed9d9111bd2
Astrid Prins <35829@ma-web.nl>
2022-10-24 13:19:06 +0200

    Edit1 py file

--- HelloYouByAstridPrins.py
+++ HelloYouByAstridPrins.py
@@ -1,5 +1,8 @@
 from time import sleep
 
+# Begin van het verhaal def
+
+
 # Introduction
 
 sleep(1)
@@ -14,9 +17,29 @@ print("In deze opdracht is er een verhaal gemaakt die gebaseerd is op ervaringen
 
 sleep(2)
 print("Wilt u het verhaal volgen?")
-volgverhaal = input("Voer in y of n ")
+volgverhaal = input("Voer in j of n ")
 
-if volgverhaal == "y" or volgverhaal == "Y" or volgverhaal == "ja" or volgverhaal == "Ja" or volgverhaal == "JA":
+if volgverhaal == "j" or volgverhaal == "J" or volgverhaal == "ja" or volgverhaal == "Ja" or volgverhaal == "JA":
+    sleep(1)
     print("Hier volgt her verhaal")
-else: 
+elif volgverhaal == "n" or volgverhaal == "N" or volgverhaal == "nee" or volgverhaal == "Nee" or volgverhaal =="Nee":
+    sleep(1)
     print("Bedankt voor uw aandacht, tot ziens!")
+
+# Begin verhaal
+if volgverhaal == "j" or volgverhaal == "J" or volgverhaal == "ja" or volgverhaal == "Ja" or volgverhaal == "JA":
+    sleep(1)
+    print("Je bent een student aan de Universiteit van Charkov in Oekraïne waar je Computer Science studeert. ")
+    sleep(1)
+    print("Je woont bij een oudere vrouw in huis waar je een hechte band mee hebt geschept.")
+    sleep(1)
+    print(" Eerder in de week was op het nieuws al te horen dat de Russen Oekraïne waren binnen gevallen, je besloot om in de stad te blijven tot het niet meer kan. ")
+    sleep(1)
+    print("Tijdens het studeren achter je computer in je kamer gaan het luchtalarm af en de radio switcht naar het noodbericht: Ga naar de schuilkelder of pak de laatste kans om te vluchten. Moldavië, Hongarije, Roemenië en Polen hebben hun grenzen open gezet voor vluchtelingen. Wees voorzichtig en God bless you all")
+    sleep(1)
+    print("Rusland heeft de aanval gericht op de stad, de troepen zijn een paar dagen verwijdert van de stad.")
+    print("")
+    print("Meteen maak je een besluit, je begint met het pakken van je spullen. ")
+    print("Wat neem je mee?")
+    
+
